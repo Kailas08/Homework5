8565a619b685f5dbc30b6d616405e41ae8d2854d
Kailas08 <lenvn.ecm@gmail.com>
2024-05-21 20:21:55 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = ['яблоко ', 'груша ', 'слива ', 'вишня ', 'мандарин ', 'черешня ']
+print(my_list)
+print(my_list[0] + my_list[-1])
+print(my_list[2:5])
+my_list[2] = ('апельсин')
+print(my_list)
+my_dict = {'яблоко': 'apple', 'груша': 'pear', 'слива': 'plum', 'вишня': 'cherry' }
+print(my_dict)
+print(my_dict['вишня'])
+my_dict['вишня'] = 2024
+print(my_dict)
+my_dict['login'] = 3**3
+print(my_dict)
